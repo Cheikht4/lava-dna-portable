890f81384d635b3f809032ee1396be6ea1d976e3
Cheikht4 <cheikhtalibouya@MacBook-Pro-de-Cheikh.local>
2026-07-25 22:51:00 +0000

    Re-apply progress fixes after resolving merge

--- lava_stem_primer.pl
+++ lava_stem_primer.pl
@@ -1508,13 +1508,11 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
   
   my $_sig_fwd_pruned = 0;
   my $_sig_fwd_evaluated = 0;
-  my $fwd_prog_file = "$options{'output_file'}_fwd_prog_$$.txt";
-  if (open my $fh, '>', $fwd_prog_file) {
-      for (1..30) { print $fh "0,0,0,0
-"; } 
-      close $fh;
-  }
-  
+  my $fwd_prog_dir = "$options{'output_file'}_fwd_prog_$$";
+  $fwd_prog_dir = "$options_r->{'output_file'}_fwd_prog_$$" if ref($options_r);
+  use File::Path qw(make_path remove_tree);
+  remove_tree($fwd_prog_dir) if -d $fwd_prog_dir;
+  make_path($fwd_prog_dir);
   my $_sig_fwd_t0   = time();
     my $_sig_fwd_done = 0;
     my $_sig_fwd_hits = 0;
@@ -1728,38 +1726,39 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
             } # End forward STEM iteration
           
           # Intra-chunk progress reporting
-          $chunk_done++;
-          if ($chunk_done % 5 == 0 || $chunk_done == $fwd_chunk_size) {
-              if (open(my $fh, '>>', $fwd_prog_file)) {
+          if ($chunk_done % 5 == 0) {
+              my $prog_file_me = "$fwd_prog_dir/chunk_$chunk_id.prog";
+              if (open(my $fh, '>', $prog_file_me)) {
                   flock($fh, 2);
-                  print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated
-";
+                  print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated\n";
                   close($fh);
+              }
                   
-                  if (open(my $fh_read, '<', $fwd_prog_file)) {
-                      my $total_done = 0;
-                      my $total_hits = 0;
-                      while(<$fh_read>) { 
-                          chomp; 
-                          next unless $_;
-                          my ($d, $h) = split /,/, $_;
-                          $total_done += $d;
-                          $total_hits += $h;
-                      }
-                      close($fh_read);
-                      
-                      if ($_LAVA_IS_TTY || 1) {
-                          my $elapsed = time() - $_sig_fwd_t0 + 0.001;
-                          my $eta = ($total_done < $innerForwardCount) ? int(($innerForwardCount - $total_done) / ($total_done / $elapsed)) : 0;
-                          my $rate = $total_done / $elapsed;
-                          printf("[LAVA-PROGRESS] Signatures Forward|%d|%d|Sig: %d|%.1f it/s|%d
-", $total_done, $innerForwardCount, $total_hits, $rate, $eta);
-                          my $old_h = select(STDOUT); $| = 1; select($old_h);
-                      }
+              my $total_done = 0;
+              my $total_hits = 0;
+              foreach my $f (glob("$fwd_prog_dir/chunk_*.prog")) {
+                  if (open(my $r, '<', $f)) {
+                      my $line = <$r>; close($r);
+                      next unless defined $line;
+                      chomp $line;
+                      my ($d, $h) = split /,/, $line;
+                      $total_done += $d // 0;
+                      $total_hits += $h // 0;
                   }
+              }
+                      
+              if ($_LAVA_IS_TTY || 1) {
+                  my $elapsed = time() - $_sig_fwd_t0 + 0.001;
+                  my $eta = ($total_done < $innerForwardCount) ? int(($innerForwardCount - $total_done) / ($total_done / $elapsed)) : 0;
+                  my $rate = $total_done / $elapsed;
+                  printf("[LAVA-PROGRESS] Signatures Forward|%d|%d|Sig: %d|%.1f it/s|%d\r", $total_done, $innerForwardCount, $total_hits, $rate, $eta);
+                  my $old_h = select(STDOUT); $| = 1; select($old_h);
               }
           }
 } # End forward inner chunk loop
+      
+      my $prog_file_me = "$fwd_prog_dir/chunk_$chunk_id.prog";
+      if (open(my $fh, '>', $prog_file_me)) { flock($fh, 2); print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated\n"; close($fh); }
       
       $pm_fwd->finish(0, {
           infos => \%chunk_infos,
@@ -1771,7 +1770,8 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
       });
   } # End chunks
   $pm_fwd->wait_all_children();
-  unlink $fwd_prog_file if -e $fwd_prog_file;
+  use File::Path qw(remove_tree);
+  remove_tree($fwd_prog_dir) if -d $fwd_prog_dir;
   
   if ($_sig_fwd_evaluated > 0) {
       my $pct = ($_sig_fwd_pruned / $_sig_fwd_evaluated) * 100;
@@ -1805,13 +1805,11 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
   
   my $_sig_rev_pruned = 0;
   my $_sig_rev_evaluated = 0;
-  my $rev_prog_file = "$options{'output_file'}_rev_prog_$$.txt";
-  if (open my $fh, '>', $rev_prog_file) {
-      for (1..30) { print $fh "0,0,0,0
-"; }
-      close $fh;
-  }
-  
+  my $rev_prog_dir = "$options{'output_file'}_rev_prog_$$";
+  $rev_prog_dir = "$options_r->{'output_file'}_rev_prog_$$" if ref($options_r);
+  use File::Path qw(make_path remove_tree);
+  remove_tree($rev_prog_dir) if -d $rev_prog_dir;
+  make_path($rev_prog_dir);
   my $_sig_rev_t0   = time();
     my $_sig_rev_done = 0;
     my $_sig_rev_hits = 0;
@@ -2025,38 +2023,39 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
             } # End reverse STEM iteration
           
           # Intra-chunk progress reporting
-          $chunk_done++;
-          if ($chunk_done % 5 == 0 || $chunk_done == $rev_chunk_size) {
-              if (open(my $fh, '>>', $rev_prog_file)) {
+          if ($chunk_done % 5 == 0) {
+              my $prog_file_me = "$rev_prog_dir/chunk_$chunk_id.prog";
+              if (open(my $fh, '>', $prog_file_me)) {
                   flock($fh, 2);
-                  print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated
-";
+                  print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated\n";
                   close($fh);
+              }
                   
-                  if (open(my $fh_read, '<', $rev_prog_file)) {
-                      my $total_done = 0;
-                      my $total_hits = 0;
-                      while(<$fh_read>) { 
-                          chomp; 
-                          next unless $_;
-                          my ($d, $h) = split /,/, $_;
-                          $total_done += $d;
-                          $total_hits += $h;
-                      }
-                      close($fh_read);
-                      
-                      if ($_LAVA_IS_TTY || 1) {
-                          my $elapsed = time() - $_sig_rev_t0 + 0.001;
-                          my $eta = ($total_done < $innerReverseCount) ? int(($innerReverseCount - $total_done) / ($total_done / $elapsed)) : 0;
-                          my $rate = $total_done / $elapsed;
-                          printf("[LAVA-PROGRESS] Signatures Reverse|%d|%d|Sig: %d|%.1f it/s|%d
-", $total_done, $innerReverseCount, $total_hits, $rate, $eta);
-                          my $old_h = select(STDOUT); $| = 1; select($old_h);
-                      }
+              my $total_done = 0;
+              my $total_hits = 0;
+              foreach my $f (glob("$rev_prog_dir/chunk_*.prog")) {
+                  if (open(my $r, '<', $f)) {
+                      my $line = <$r>; close($r);
+                      next unless defined $line;
+                      chomp $line;
+                      my ($d, $h) = split /,/, $line;
+                      $total_done += $d // 0;
+                      $total_hits += $h // 0;
                   }
+              }
+                      
+              if ($_LAVA_IS_TTY || 1) {
+                  my $elapsed = time() - $_sig_rev_t0 + 0.001;
+                  my $eta = ($total_done < $innerReverseCount) ? int(($innerReverseCount - $total_done) / ($total_done / $elapsed)) : 0;
+                  my $rate = $total_done / $elapsed;
+                  printf("[LAVA-PROGRESS] Signatures Reverse|%d|%d|Sig: %d|%.1f it/s|%d\r", $total_done, $innerReverseCount, $total_hits, $rate, $eta);
+                  my $old_h = select(STDOUT); $| = 1; select($old_h);
               }
           }
 } # End reverse inner chunk loop
+      
+      my $prog_file_me = "$rev_prog_dir/chunk_$chunk_id.prog";
+      if (open(my $fh, '>', $prog_file_me)) { flock($fh, 2); print $fh "$chunk_done,$chunk_hits,$chunk_pruned,$chunk_evaluated\n"; close($fh); }
       
       $pm_rev->finish(0, {
           infos => \%chunk_infos,
@@ -2068,7 +2067,8 @@ our $_LAVA_IS_TTY = -t STDERR ? 1 : 0;
       });
   } # End chunks
   $pm_rev->wait_all_children();
-  unlink $rev_prog_file if -e $rev_prog_file;
+  use File::Path qw(remove_tree);
+  remove_tree($rev_prog_dir) if -d $rev_prog_dir;
   
   if ($_sig_rev_evaluated > 0) {
       my $pct = ($_sig_rev_pruned / $_sig_rev_evaluated) * 100;
